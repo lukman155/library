76783eea63174422384fc6002dbf62e81f65860a
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-02-09 10:56:55 +0100

    Fix: Linters Errors

--- book.rb
+++ book.rb
@@ -2,7 +2,7 @@ class Book
   attr_reader :rentals
   attr_accessor :title, :author
 
-  def initialise(title, author)
+  def initialise(title, _author)
     @title = title
     @author = title
     @rentals = []
