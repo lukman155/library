2e05a18e487b25e6c6ea88fe95193a5d3312c07d
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-02-09 10:53:16 +0100

    Add: Associations

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require 'capitalize_decorator'
 require 'trimmer_decorator'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :age, :name
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -12,6 +12,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services
