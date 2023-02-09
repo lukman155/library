0d3cbdfefa3ef62fac1278f89a74216d8469c1ce
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-02-09 08:25:04 +0100

    Add: Decorator

---
+++ capitalize_decorator.rb
@@ -0,0 +1,7 @@
+require './decorator.rb'
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.upcase
+  end
+end
--- person.rb
+++ person.rb
@@ -1,8 +1,13 @@
-class Person
+require './nameable.rb'
+require 'capitalize_decorator.rb'
+require 'trimmer_decorator.rb'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :age, :name
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -11,6 +16,10 @@ class Person
 
   def can_use_services
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
